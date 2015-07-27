72f8cc7a1168a98dd5e3943a31fa38f65ee02e37
Cheuk <Cheuk1991@qq.com>
2015-07-27 15:41:04 +0800

    .

--- Application/Cache/Compile/Home/Home/index.php
+++ Application/Cache/Compile/Home/Home/index.php
@@ -1,10 +1,11 @@
+<?php defined('DP_VER') or exit();?>
 <html>
 <head>
 <meta charset='utf-8'>
 <title>欢迎使用！</title>
 </head>
 <body>
-<h1>{:="Hello ".$name;}</h1>
+<h1>{:="Hello ".$name}</h1>
 <h1>Do It If You Want</h1>
 </body>
 </html>
